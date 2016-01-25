0e41af6bfa10bf53017d5f1eb144a8d602746327
Dmitry Zuev <mail@dzuev.ru>
2016-01-26 01:31:16 +0300

    Finishing StringDecoder service object which parses human readable number strings

--- app/services/string_decoder.rb
+++ app/services/string_decoder.rb
@@ -1,6 +1,51 @@
+# Convert human entered string into number
 class StringDecoder
+  POWERS = {
+    'трлн' => 10**12,
+    'триллион' => 10**12,
+    'триллиона' => 10**12,
+    'триллионов' => 10**12,
+    'млрд' => 10**9,
+    'миллиард' => 10**9,
+    'миллиарда' => 10**9,
+    'миллиардов' => 10**9,
+    'млн' => 10**6,
+    'миллион' => 10**6,
+    'миллиона' => 10**6,
+    'миллионов' => 10**6,
+    'т' => 10**3,
+    'тыс' => 10**3,
+    'тысяча' => 10**3,
+    'тысячи' => 10**3,
+    'тысяч' => 10**3
+  }.freeze
 
   def call(input)
-    input.to_i
+    return input.gsub(/[\s,]/, '').to_f if float? input.gsub(/[\s,]/, '')
+
+    result = 0
+
+    # Split string by numbers and words
+    # inputs = input.gsub(/(?<=[\d])(?=[а-яА-Я])/, ' ').split(' ')
+    # inputs.each_with_index do |i, index|
+    #   if POWERS[i] && float?(inputs[index-1].to_s) && index > 0
+    #     result += inputs[index-1].to_f * POWERS[i]
+    #   end
+    # end
+
+    input.gsub(/(?<=[\d])(?=[а-яА-Я])/, ' ')
+         .split(' ')
+         .map{ |i| POWERS[i]? POWERS[i].to_f : i.to_f }
+         .each_slice(2) do |slice|
+      result += slice.inject(:*)
+    end
+
+    result
+  end
+
+  private
+
+  def float?(input)
+    input[/\A[\d]+[\.]?[\d]*\z/] == input
   end
 end
--- spec/services/string_decoder_spec.rb
+++ spec/services/string_decoder_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe StringDecoder do
 
   it 'decodes numeric string' do
     input = '1231412353'
-    expect(decoder.call(input)).to equal(1231412353)
+    expect(decoder.call(input)).to equal(1231412353.0)
   end
 
   it 'decodes numeric string with spaces' do
     input = '1 412 935 315'
-    expect(decoder.call(input)).to equal(1412935315)
+    expect(decoder.call(input)).to equal(1412935315.0)
   end
 
   it 'decodes numeric string with punctuation' do
@@ -20,16 +20,16 @@ RSpec.describe StringDecoder do
 
   it 'decodes string with млрд млн тыс' do
     input = '1 млрд 25 млн'
-    expect(decoder.call(input)).to equal(1025000000)
+    expect(decoder.call(input)).to equal(1025000000.0)
   end
 
   it 'decodes string with миллиард миллион тысяча' do
     input = '1 миллиард 149 миллионов 51 тысяча'
-    expect(decoder.call(input)).to equal(1149051000)
+    expect(decoder.call(input)).to equal(1149051000.0)
   end
 
   it 'decodes string with punctuatuin and words' do
-    input = '1,44 миллиардов'
-    expect(decoder.call(input)).to equal(1440000000)
+    input = '1.44 миллиарда'
+    expect(decoder.call(input)).to equal(1440000000.0)
   end
 end
