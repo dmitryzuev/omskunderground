eb02ff611fc1d357dd7d31833eaae74e3a1496e7
Dmitry Zuev <mail@dzuev.ru>
2016-01-30 01:09:13 +0300

    Write tests for Record model

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :record do
     sum { rand*rand(1000000000000) }
     currency 'RUR'
-    undergrounds { sum/24000000000 }
-    reason 'Clockwork Orange'
+    undergrounds { sum/METRO_COST }
+    reason { FFaker::Product.product }
     exchange_rate 1.0
   end
-
+  
 end
