fbe6ec99efd73417732c651ceef04a7c86343961
Dmitry Zuev <mail@dzuev.ru>
2016-02-13 03:59:52 +0300

    StringDecoder modifications

--- app/services/string_decoder.rb
+++ app/services/string_decoder.rb
@@ -21,17 +21,17 @@ class StringDecoder
   }.freeze
 
   def call(input)
+    input = comma_to_dot(input.mb_chars.downcase.to_s)
+
     return input.gsub(/[\s,]/, '').to_f if float? input.gsub(/[\s,]/, '')
 
     result = 0
-
     input.gsub(/(?<=[\d])(?=[а-яА-Я])/, ' ')
          .split(' ')
          .map { |i| POWERS[i] ? POWERS[i].to_f : i.to_f }
          .each_slice(2) do |slice|
       result += slice.inject(:*)
     end
-
     result
   end
 
@@ -39,5 +39,15 @@ class StringDecoder
 
   def float?(input)
     input[/\A[\d]+[\.]?[\d]*\z/] == input
+  end
+
+  # Change all commas to dots, remove all except last one
+  # It used to make sure '1,231.41', '1231.41' & '1231,41' are the same
+  def comma_to_dot(input)
+    return input if !input.include?('.') && !input.include?(',')
+    return input.delete(',') if input.include?(',') && !input.include?('.')
+
+    dots = input.tr(',', '.').split('.')
+    "#{dots[0..-2].join}.#{dots[-1]}"
   end
 end
